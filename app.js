5321cfcb6ef31db85ef71ca367a287b48ece45b3
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-07-30 23:08:00 +0200

    Dodanie ruchomych orbit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const scene = new THREE.Scene();
 
 //jest kilka rodzajów kamer - w dokumentacji jest ich więcej. Perspective Camera przjmuje 4 parametry - kąt widzenia,
 //wymiary kadru, odległośc w jakiej obiekt może się znajdowac najbliżej kamery i najdalej
-const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 //musze ustawić kamerę bo domyślnie jest na środku, tak jak sam obiekt
 camera.position.set(3, 0, 15);
@@ -47,51 +47,49 @@ const createObject = (r = 1, color) => {
 
 const planet = createObject(2, blueColor);
 
-const moon1 = createObject(0.5, greyColor);
-moon1.position.set(5, 2, -5);
-const moon2 = createObject(0.3, purpleColor);
-moon2.position.set(7, -1, 3);
+const createMoon = (r, color) => {
+    const moon = createObject(r, color);
+    const pivot = new THREE.Object3D();
+    pivot.add(moon);
+    return {
+        moon,
+        pivot
+    }
+}
 
-// x = 100  // center
-// y = 50   // center
-// r = 50   // radius
-// a = 0    // angle (from 0 to Math.PI * 2)
-//
-// function rotate(a) {
-//
-//     var px = x + r * Math.cos(a); // <-- that's the maths you need
-//     var py = y + r * Math.sin(a);
-//
-//     document.querySelector('#point').style.left = px + "px";
-//     document.querySelector('#point').style.top = py + "px";
-// }
-//
-//
-// setInterval(function() {
-//     a = (a + Math.PI / 360) % (Math.PI * 2);
-//     rotate(a);
-// }, 5);
+const moon1 = createMoon(0.5, greyColor);
+moon1.moon.position.set(5, 0, 0);
+
+const moon2 = createMoon(0.2, purpleColor);
+moon2.moon.position.set(8, -1, 2);
+scene.add(moon1.pivot, moon2.pivot);
+
+moon1.pivot.rotation.x = 60;
+moon2.pivot.rotation.y = 25;
 
 //tworzę sobie oświetlenie sceny
-const light = new THREE.PointLight(yellowColor, 2.5);
+const light = new THREE.PointLight(yellowColor, 5.5);
 const light2 = new THREE.PointLight(whiteColor, 2);
 
 //ustawiam położenie światła
-light.position.set(-20, -10, 10);
-light2.position.set(40, 0, 20);
+light.position.set(-20, 40, 10);
+light2.position.set(40, 10, 20);
 
 // const ctx = moon1.getContext('2d');
 
 //muszę dodać bryłę i światło do sceny
-scene.add(planet, moon1, moon2, light, light2);
+scene.add(planet, moon1, moon2, light);
+planet.add(light2);
 
 const loop = () => {
     //obejrzeć odcinek romana o canvasie
     //po każdym wywołaniu funkcji animate animacja się zapętli, bo mam tu callback i po kazdym odświeżeniu ramki
     //ta funkcja będzie wywoływana na nowo
     requestAnimationFrame(loop);
-    planet.rotation.x += 0.05;
-    planet.rotation.y += 0.01;
+    moon1.pivot.rotation.y += 0.01;
+    moon2.pivot.rotation.y -= 0.02;
+    // planet.rotation.x += 0.05;
+    planet.rotation.y -= 0.01;
     renderer.render(scene, camera);
 }
 
